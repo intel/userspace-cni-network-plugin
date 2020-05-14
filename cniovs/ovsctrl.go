f852f334913d74219239560106b6e457dafb2e26
Martin Klozik <martinx.klozik@intel.com>
2020-05-14 05:57:17 +0100

    Update license boilerplate
    Update license of ovsctrl.go to reflect development in previous years.
    
    Signed-off-by: Martin Klozik <martinx.klozik@intel.com>

--- cniovs/ovsctrl.go
+++ cniovs/ovsctrl.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Intel Corp.
+// Copyright 2018-2020 Red Hat, Intel Corp.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
