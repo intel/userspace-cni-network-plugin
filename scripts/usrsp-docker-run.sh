ca6a50e9482416bea653a2240bc325088e289975
killianmuldoon <killian.muldoon@intel.com>
2020-09-22 11:37:14 +0100

    edited shellscripts for static analysis

--- scripts/usrsp-docker-run.sh
+++ scripts/usrsp-docker-run.sh
@@ -17,23 +17,23 @@
 #
 
 scriptpath=$GOPATH/src/github.com/containernetworking/cni/scripts
-echo $scriptpath
+echo "$scriptpath"
 
-contid=$(docker run -d --net=none $@ /bin/sleep 10000000)
-pid=$(docker inspect -f '{{ .State.Pid }}' $contid)
+contid=$(docker run -d --net=none "$@" /bin/sleep 10000000)
+pid=$(docker inspect -f '{{ .State.Pid }}' "$contid")
 netnspath=/proc/$pid/ns/net
 
-$scriptpath/exec-plugins.sh add $contid $netnspath
+"$scriptpath"/exec-plugins.sh add "$contid" "$netnspath"
 
 function cleanup() {
-	$scriptpath/exec-plugins.sh del $contid $netnspath
-	docker rm -f $contid >/dev/null
+	"$scriptpath"/exec-plugins.sh del "$contid" "$netnspath"
+	docker rm -f "$contid" >/dev/null
 }
 trap cleanup EXIT
 
 docker run \
  -v /var/lib/cni/usrspcni/shared:/var/lib/cni/usrspcni/shared:rw \
- -v /var/lib/cni/usrspcni/$contid:/var/lib/cni/usrspcni/data:rw \
+ -v /var/lib/cni/usrspcni/"$contid":/var/lib/cni/usrspcni/data:rw \
  --device=/dev/hugepages:/dev/hugepages \
- --net=container:$contid $@
+ --net=container:"$contid" "$@"
 
