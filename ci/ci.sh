b1ecd1c1b41fdb22c8f2b0d4c485edcf4ae191e8
Eoghan Conlon <eoghan.conlon@intel.com>
2024-05-28 15:28:19 +0100

    Fix. OpenSSF errors using wget2
    This won't work unless we were to move to selfhosted runners, Github's runners don't have wget2 installed on them by default
    
    Signed-off-by: Eoghan Conlon <eoghan.conlon@intel.com>

--- ci/ci.sh
+++ ci/ci.sh
@@ -28,10 +28,7 @@ export PATH="${PATH}:${HOME}/go/bin"
 echo "export PATH=\"${PATH}:${HOME}/go/bin/:${HOME}.local/bin/\"" >>~/.bashrc
 go install sigs.k8s.io/kind@v0.20.0
 
-wget -q https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl -O "${HOME}/go/bin/kubectl"
-wget -q https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl.sha256 -O "${HOME}/kubectl.sha256"
-cat_sha=$(cat "${HOME}/kubectl.sha256")
-echo "${cat_sha} ${HOME}/go/bin/kubectl" | sha256sum --check
+wget2 -s --signature-extensions .sha256 -o "${HOME}/go/bin/kubectl" https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl
 chmod +x "${HOME}/go/bin/kubectl"
 }
 
