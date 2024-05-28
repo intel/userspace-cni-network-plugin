1191ea4a08490b48c884900738f8875ccceee2ba
Eoghan Conlon <eoghan.conlon@intel.com>
2024-05-28 10:14:32 +0100

    Fix. Attempt to fix Shellcheck check
    Signed-off-by: Eoghan Conlon <eoghan.conlon@intel.com>

--- ci/ci.sh
+++ ci/ci.sh
@@ -30,7 +30,8 @@ go install sigs.k8s.io/kind@v0.20.0
 
 wget -q https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl -O "${HOME}/go/bin/kubectl"
 wget -q https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl.sha256 -O "${HOME}/kubectl.sha256"
-echo "$(cat ${HOME}/kubectl.sha256)  ${HOME}/go/bin/kubectl" | sha256sum --check
+cat_sha=$(cat "${HOME}/kubectl.sha256")
+echo ${cat_sha}  "${HOME}/go/bin/kubectl" | sha256sum --check
 chmod +x "${HOME}/go/bin/kubectl"
 }
 
